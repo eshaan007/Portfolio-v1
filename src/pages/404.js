cfc5ac941efb2d7796718c0734ad0c70ca46eeb9
eshaan007 <42370985+eshaan007@users.noreply.github.com>
2020-05-05 16:25:30 +0530

    Added 404 Error Page

---
+++ src/pages/404.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import { Link } from 'gatsby'
+
+import Layout from '../components/layout'
+
+const NotFound = () => {
+    return (
+        <Layout>
+            <h1> Page not found</h1>
+            <p><Link to="/">Head Home</Link></p>
+        </Layout>
+    )
+}
+
+export default NotFound
