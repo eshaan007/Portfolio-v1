f5d7d64567c4c1051d833463b498699c6c587cfc
eshaan007 <42370985+eshaan007@users.noreply.github.com>
2020-05-05 11:00:30 +0530

    Added Blog Posts

--- src/templates/blog.js
+++ src/templates/blog.js
@@ -1,10 +1,25 @@
 import React from 'react'
 import Layout from '../components/layout'
+import { graphql } from 'gatsby'
 
-const Blog = () => {
+export const query = (graphql`
+    query($slug: String!) {
+        markdownRemark(fields: { slug: { eq: $slug } }) {
+            frontmatter {
+                title
+                date
+            }
+            html
+        }
+    }
+`)
+
+const Blog = (props) => {
     return (
         <Layout>
-            This is the blog template
+            <h1>{props.data.markdownRemark.frontmatter.title}</h1>
+            <p>{props.data.markdownRemark.frontmatter.date}</p>
+            <div dangerouslySetInnerHTML = {{ __html: props.data.markdownRemark.html}}></div>
         </Layout>
     )
 }
