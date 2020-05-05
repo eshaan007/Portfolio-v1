ba709c1fd301a05d9b5c3af15508245fb652d976
eshaan007 <42370985+eshaan007@users.noreply.github.com>
2020-05-05 17:34:48 +0530

    Update

--- src/pages/index.js
+++ src/pages/index.js
@@ -9,7 +9,6 @@ const IndexPage = () => {
             <Head title="Home"/>
             <h1> Hello ! </h1>
             <h2> I'm Eshaan, A Web Developer/ Student @ IIITV/ Tech Enthusiast</h2>
-            <p> Need a dev? <Link to="/contact">Contact me for details!!</Link></p>
         </Layout>
     )
 }
