7607b950cc04f424d580aa3986ceb00c56be2b82
eshaan007 <42370985+eshaan007@users.noreply.github.com>
2020-05-06 10:24:46 +0530

    Media Queries Added

--- src/pages/index.js
+++ src/pages/index.js
@@ -11,8 +11,9 @@ const IndexPage = () => {
                 <h4 className="intro">Hi, my name is </h4>
                 <h2 className="name">Eshaan Khurana.</h2>
                 <h2 className="aftername"> I build things for the web.</h2>
-                <h4 className="desc">I'm a Sophomore @ IIIT, Vadodara specializing in building websites using various frameworks. Check my Profile <a class="highlight-link" href="https://github.com/eshaan007" target="_blank">@eshaan007</a>👩🏻‍💻</h4>
-                <p className=""><a href="mailto:eshaany2k@gmail.com" class="arrow-link">Get in touch ➡️</a></p>
+                <h4 className="desc">I'm a Sophomore @ IIIT, Vadodara specializing in building websites using various frameworks.
+                Check my Profile <a className="highlight-link" href="https://github.com/eshaan007" target="_blank">@eshaan007</a>👩🏻‍💻</h4>
+                <p className=""><a href="mailto:eshaany2k@gmail.com" className="arrow-link">Get in touch ➡️</a></p>
             </article> 
         </Layout>
     )
