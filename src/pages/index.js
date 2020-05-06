7169593dc63ceec85510f7cd7e98c592423910a6
eshaan007 <42370985+eshaan007@users.noreply.github.com>
2020-05-06 08:20:46 +0530

    Home-page

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,10 +8,11 @@ const IndexPage = () => {
         <Layout>
             <Head title="Home"/>
             <article>
-                <h1><strong>Hello!</strong> 👋</h1>
-                <p>I'm <strong>Eshaan Khurana</strong>, a Web-Developer focused on building beautiful interfaces. 
-                I am currently a Sophomore @IIIT Vadodara. Check my Profile <a class="highlight-link" href="https://github.com/eshaan007" target="_blank">@eshaan007</a>👩🏻‍💻</p>
-                <p><a href="mailto:eshaany2k@gmail.com" class="arrow-link">Get in touch ➡️</a></p>
+                <h4 className="intro">Hi, my name is </h4>
+                <h2 className="name">Eshaan Khurana.</h2>
+                <h3 className="aftername"> A Web-Developer focused on building beautiful interfaces.</h3>
+                <h4 className="desc">I am currently a Sophomore @IIIT Vadodara. Check my Profile <a class="highlight-link" href="https://github.com/eshaan007" target="_blank">@eshaan007</a>👩🏻‍💻</h4>
+                <p className=""><a href="mailto:eshaany2k@gmail.com" class="arrow-link">Get in touch ➡️</a></p>
             </article> 
         </Layout>
     )
