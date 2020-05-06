e2269ab74d6e13a9d4233adb85285e7d03de2acf
eshaan007 <42370985+eshaan007@users.noreply.github.com>
2020-05-06 09:13:35 +0530

    Deleted Unwanted CSS

--- src/pages/index.js
+++ src/pages/index.js
@@ -10,8 +10,10 @@ const IndexPage = () => {
             <article>
                 <h4 className="intro">Hi, my name is </h4>
                 <h2 className="name">Eshaan Khurana.</h2>
-                <h3 className="aftername"> A Web-Developer focused on building beautiful interfaces.</h3>
-                <h4 className="desc">I am currently a Sophomore @IIIT Vadodara. Check my Profile <a class="highlight-link" href="https://github.com/eshaan007" target="_blank">@eshaan007</a>👩🏻‍💻</h4>
+                <h2 className="aftername"> I build things for the web.</h2>
+                <div className="contain-desc">
+                    <h4 className="desc">I'm a Sophomore @ Indian Institute of Information Technology, Vadodara specializing in building websites using various frameworks. Check my Profile <a class="highlight-link" href="https://github.com/eshaan007" target="_blank">@eshaan007</a>👩🏻‍💻</h4>
+                </div>
                 <p className=""><a href="mailto:eshaany2k@gmail.com" class="arrow-link">Get in touch ➡️</a></p>
             </article> 
         </Layout>
